5876c31c17f920c2824677116ffe0ff3e728843f
Tolga <tkoseoglu@live.com>
2015-07-30 16:13:11 -0600

    Added Grunt and dist folder

---
+++ dist/js/watg-angular-feedback.js
@@ -0,0 +1,165 @@
+/**
+ * Created by Kemal on 07/30/15.
+ */
+var feedbackModule = angular.module('feedbackModule', ['ngRoute', 'ngSanitize'])
+    .config(function ($routeProvider, $httpProvider) {
+
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+
+        $routeProvider
+            .when('/',
+            {
+                templateUrl: 'app/views/feedbackView.html',
+                controller: 'feedbackController'
+            })
+            .otherwise({redirectTo: '/'});
+    });
+
+"use strict";
+feedbackModule.controller('feedbackController',
+    function feedbackController($scope, feedbackService) {
+        $scope.header = 'Feedback';
+
+        $scope.projectName = "eMail Campaign App";
+        $scope.getProjectDetailsUrl = 'http://itstage.watg.com/watgxapirest/api/Feedback/GetProjectDetails/';
+        $scope.submitProjectFeedbackUrl = 'http://itstage.watg.com/watgxapirest/api/Feedback/AddProjectFeedback';
+        $scope.userFullName = "Tolga Koseoglu";
+
+    }
+);
+
+
+/**
+ * Created by Kemal on 07/30/15.
+ */
+feedbackModule.directive("watgFeedback", function (feedbackService) {
+    var controller = ['$scope', function ($scope) {
+
+        $scope.header = 'Feedback';
+        $scope.isBusySubmittingFeedback = false;
+        $scope.showConfirmation = false;
+        $scope.form = {};
+        $scope.feedbackItem = {
+            feedback: '',
+            applicationId: 0,
+            applicationName: '',
+            applicationDescription: '',
+            applicationVersion: '',
+            vendor: '',
+            platform: '',
+            userAgent: '',
+            screenResolution: '',
+            rating: null
+        }
+        $scope.max = 5;
+        $scope.stars = [];
+        $scope.ratingValue = 3;
+
+        $scope.getProjectDetails = function () {
+            $scope.isBusy = true;
+            feedbackService.getProjectDetails($scope.getUrl + '/' + $scope.projectName).then(function (result) {
+                $scope.feedbackItem.applicationId = result.Id;
+                $scope.feedbackItem.applicationName = result.ProjectName;
+                $scope.feedbackItem.applicationDescription = result.ProjectDescription;
+                $scope.feedbackItem.applicationVersion = result.Version;
+                $scope.isBusy = false;
+            });
+        };
+        $scope.submitFeedback = function () {
+
+            $scope.isBusySubmittingFeedback = true;
+
+            $scope.feedbackItem.vendor = navigator.vendor;
+            $scope.feedbackItem.platform = navigator.platform;
+            $scope.feedbackItem.userAgent = navigator.userAgent;
+            $scope.feedbackItem.screenResolution = screen.width + '*' + screen.height;
+            $scope.feedbackItem.rating = $scope.ratingValue;
+
+            feedbackService.addProjectFeedback($scope.feedbackItem, $scope.submitUrl).then(function (result) {
+
+                console.log(result);
+                var transactionResult = result;
+                $scope.showConfirmation = true;
+                $scope.isBusySubmittingFeedback = false;
+
+            });
+        };
+        $scope.toggle = function (index) {
+            $scope.ratingValue = index + 1;
+        };
+        $scope.$watch('ratingValue', function (oldValue, newValue) {
+            if (newValue) {
+                updateStars();
+            }
+        });
+        function updateStars() {
+
+            $scope.stars = [];
+            for (var i = 0; i < $scope.max; i++) {
+                $scope.stars.push({
+                    filled: i < $scope.ratingValue
+                });
+            }
+        };
+
+
+        $scope.getProjectDetails();
+    }];
+    return {
+        restrict: 'E',
+        templateUrl: 'app/templates/feedbackTemplate.html',
+        scope: {
+            projectName: '=',
+            getUrl: '=',
+            submitUrl: '=',
+            userFullName: '='
+        },
+        controller: controller,
+        link: function (scope, element) {
+
+            console.log(scope.projectName);
+            console.log(scope.getUrl);
+            console.log(scope.submitUrl);
+            console.log(scope.userFullName);
+
+        }
+
+    }
+});
+/**
+ * Created by Kemal on 07/30/15.
+ */
+'use strict';
+feedbackModule.factory('feedbackService', function ($http, $log, $q, $rootScope) {
+
+    return {
+        getProjectDetails: function (url) {
+            console.log(url);
+            return $http({
+                method: 'GET',
+                withCredentials: true,
+                url: url
+            }).
+                then(function (response) {
+                    return response.data;
+                });
+        },
+        addProjectFeedback: function (vm, url) {
+            console.log(vm);
+            console.log(url);
+            return $http({
+                method: 'POST',
+                data: $.param(vm),
+                withCredentials: true,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                url: url
+            }).
+                then(function (response) {
+                    return response.data;
+                });
+        }
+    }
+
+});
---
+++ gruntfile.js
@@ -0,0 +1,132 @@
+/**
+ * Created by Kemal on 07/30/15.
+ */
+/*
+ * CONCAT APP JS
+ * CONCAT ASSET CSS
+ * CONCAT VENDOR JS
+ * CONCAT VENDOR CSS
+ * WATCH APP JS
+ * WATCH ASSET CSS
+ * COPY ICONS
+ * COPY FONTS
+ * CONDITIONAL GRUNT DEV VS PROD
+ *
+ * NICE TO HAVS
+ * NOT HAVING TO EXPLICTLY INCLUDE VENDOR .JS OR .CSS
+ *
+ * NPM, bower, grunt
+ *
+ */
+
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            app: {
+                src: ['src/app/**/*.js'],
+                dest: 'dist/js/watg-angular-feedback.js'
+            },
+            vendor: {
+                src: [
+                    'bower_components/jquery/dist/jquery.min.js',
+                    'bower_components/bootstrap/dist/js/bootstrap.min.js',
+                    'bower_components/moment/min/moment.min.js',
+                    'bower_components/angular/angular.min.js',
+                    'bower_components/angular-sanitize/angular-sanitize.min.js',
+                    'bower_components/angular-route/angular-route.min.js',
+                    'bower_components/angular-animate/angular-animate.min.js'
+                ],
+                dest: 'dist/js/vendor.min.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                mangle: false
+            },
+            app: {
+                files: {
+                    'dist/js/watg-angular-feedback.min.js': ['dist/js/watg-angular-feedback.js']
+                }
+            }
+        },
+        concat_css: {
+            assets: {
+                src: ["src/assets/watg-angular-feedback.css"],
+                dest: "dist/css/watg-angular-feedback.css"
+            }
+        },
+        cssmin: {
+            options: {
+                keepSpecialComments: 0
+            },
+            assets: {
+                files: {
+                    'dist/css/watg-angular-feedback.min.css': ['dist/css/watg-angular-feedback.css']
+                }
+            },
+            vendor: {
+                files: {
+                    'dist/css/vendor.min.css': [
+                        'bower_components/bootstrap/dist/css/bootstrap.css',
+                        'bower_components/fontawesome/css/font-awesome.css'
+                    ]
+                }
+            }
+        },
+        watch: {
+            files: ['src/app/**/*.js', 'src/assets/*.css'],
+            tasks: ['concat', 'uglify', 'concat_css', 'cssmin']
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        src: ['bower_components/fontawesome/fonts/*', 'bower_components/bootstrap/fonts/*'],
+                        dest: 'dist/fonts/',
+                        filter: 'isFile',
+                        flatten: true
+                    },
+                    {
+                        expand: true,
+                        src: ['bower_components/footable/css/fonts/*'],
+                        dest: 'dist/css/fonts/',
+                        filter: 'isFile',
+                        flatten: true
+                    },
+                    {
+                        expand: true,
+                        src: ['bower_components/fuelux/dist/fonts/*'],
+                        dest: 'dist/css/fonts/',
+                        filter: 'isFile',
+                        flatten: true
+                    },
+                    {
+                        expand: true,
+                        src: ['content/images/*', 'bower_components/jquery-ui/themes/base/images/*'],
+                        dest: 'dist/css/images/',
+                        filter: 'isFile',
+                        flatten: true
+                    }
+                ]
+            },
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-concat-css');
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['concat', 'uglify', 'concat_css', 'cssmin', 'copy', 'watch']);
+
+    //grunt.registerTask('prod', ['uglify']);
+    //grunt.registerTask('dev', ['watch']);
+
+};
