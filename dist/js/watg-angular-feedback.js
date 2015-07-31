06632f163cad17950f167268fdd0886f4b5cd809
Tolga <tkoseoglu@live.com>
2015-07-30 20:53:50 -0600

    Changed user name

--- dist/js/watg-angular-feedback.js
+++ dist/js/watg-angular-feedback.js
@@ -22,13 +22,10 @@ watgFeedbackModule.controller('feedbackTestController',
     function feedbackTestController($scope) {
         $scope.header = 'Feedback';
 
-
         $scope.projectName = "eMail Campaign App";
         $scope.getProjectDetailsUrl = 'http://itstage.watg.com/watgxapirest/api/Feedback/GetProjectDetails/';
         $scope.submitProjectFeedbackUrl = 'http://itstage.watg.com/watgxapirest/api/Feedback/AddProjectFeedback';
-        $scope.user = {
-            FullName: 'Tolga Koseoglu'
-        }
+        $scope.userFullName = 'Tolga Koseoglu';
 
     }
 );
