02b224a08fe84a6f121396fe9ca13c659aca9408
Kemal Tolga Koseoglu <tolga.k@outlook.com>
2017-04-23 19:33:02 -0600

    Richtext and Fileupload updates

--- dist/js/watg-angular-feedback.js
+++ dist/js/watg-angular-feedback.js
@@ -58,12 +58,85 @@ angular.module('watgFeedbackModule.const', [])
                 Files: [],
                 MaxFileSize: 1024 * 1024 * 5,
                 MaxNumberOfFiles: 5,
-                AllowedFileExtensions: "png,jpg,doc,docx,pdf"
+                AllowedFileExtensions: "png,jpg,doc,docx,pdf",
+                Button: {
+                    Style: "btn-primary btn-sm"
+                }
             };
             $scope.feedbackRichtextConfig = {
                 height: $scope.feedbackInputHeight,
-                multiLine: true,
-                bootstrapCssPath: boostrapCssPath,
+                multiLine: true, //default true           
+                fontSizes: [{
+                    fontSizeName: 'Huge',
+                    fontSize: 7
+                }, {
+                    fontSizeName: 'Big',
+                    fontSize: 5
+                }, {
+                    fontSizeName: 'Normal',
+                    fontSize: 3
+                }, {
+                    fontSizeName: 'Small',
+                    fontSize: 1
+                }],
+                fontFamilies: [{
+                    fontName: 'Arial'
+                }, {
+                    fontName: 'Calibri'
+                }, {
+                    fontName: 'Time New Roman'
+                }, {
+                    fontName: 'Palatino Linotype'
+                }],
+                colors: [{
+                    colorName: 'Black',
+                    colorValue: '000000'
+                }, {
+                    colorName: 'Silver',
+                    colorValue: 'C0C0C0'
+                }, {
+                    colorName: 'Gray',
+                    colorValue: '808080'
+                }, {
+                    colorName: 'White',
+                    colorValue: 'FFFFFF'
+                }, {
+                    colorName: 'Maroon',
+                    colorValue: '800000'
+                }, {
+                    colorName: 'Red',
+                    colorValue: 'FF0000'
+                }, {
+                    colorName: 'Purple',
+                    colorValue: '800080'
+                }, {
+                    colorName: 'Fuchsia',
+                    colorValue: 'FF00FF'
+                }, {
+                    colorName: 'Green',
+                    colorValue: '008000'
+                }, {
+                    colorName: 'Lime',
+                    colorValue: '00ff00'
+                }, {
+                    colorName: 'Olive',
+                    colorValue: '808000'
+                }, {
+                    colorName: 'Yellow',
+                    colorValue: 'ffff00'
+                }, {
+                    colorName: 'Navy',
+                    colorValue: '000080'
+                }, {
+                    colorName: 'Blue',
+                    colorValue: '0000FF'
+                }, {
+                    colorName: 'Teal',
+                    colorValue: '008080'
+                }, {
+                    colorName: 'Aqua',
+                    colorValue: '00ffff'
+                }],
                 showVariablesSelector: true,
                 showFontSelector: true,
                 showFontSizeSelector: true,
@@ -84,7 +157,10 @@ angular.module('watgFeedbackModule.const', [])
                 showRedo: true,
                 showInsertLink: true,
                 showRemoveLink: true,
-                showSourceCode: false
+                showSourceCode: false,
+                Button: {
+                    Style: "btn-primary btn-sm"
+                }
             };
         };
         $scope.getAppDevProjectByProjectName = function() {
